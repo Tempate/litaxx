671cd294a6cb525bdb9125fea3a732b4e3a7bc46
Tempate <ddiazq28@gmail.com>
2020-10-18 20:45:23 +0200

    Moved button-click-handling to js

--- client/resources/js/game/clientHandler.js
+++ client/resources/js/game/clientHandler.js
@@ -1,5 +1,12 @@
 const ServerHandler = require("./serverHandler");
 
+const buttons = {
+    "resign": document.querySelector("#resign"),
+    "draw":   document.querySelector("#draw"),
+
+    "previousMove": document.querySelector("#previous-move"),
+    "nextMove": document.querySelector("#next-move")
+};
 
 // Disconnect a user from the room when he unloads the page
 window.addEventListener('beforeunload', function(_) {
@@ -30,10 +37,18 @@ window.addEventListener('keydown', function(e) {
     }
 });
 
-function resign() {
-    ServerHandler.socket.emit('resign');
-}
+buttons.previousMove.addEventListener('click', function(_) {
+    ServerHandler.previousMove();
+});
 
-function offerDraw() {
+buttons.nextMove.addEventListener('click', function(_) {
+    ServerHandler.nextMove();
+});
+
+buttons.resign.addEventListener('click', function(_) {
+    ServerHandler.socket.emit('resign');
+});
+
+buttons.draw.addEventListener('click', function(_) {
     ServerHandler.socket.emit('offer_draw');
-}
+});
